b4b699bbdff7009b3d38c63211733ee4e2e46a05
Mariel Huerta Escobar <marielhuerta818@gmail.com>
2026-02-04 19:02:57 -0600

    basic pandas

--- 13.py
+++ 13.py
@@ -6,7 +6,7 @@ ventas = [10, 50, 30]
 haz:
 imprime el máximo
 imprime el índice del máximo
-solo eso.  """
+solo eso.  
 
 
 import pandas as pd
@@ -24,4 +24,37 @@ df["ventas"].max
 
 print(df)
 print(df["ventas"].idxmax())
-print(df["ventas"].max())
+print(df["ventas"].max()) """    
+
+
+
+
+
+
+
+
+""" ENCONTRAR LA FILA DEL MÁXIMO:
+Este es otro ejercicio:
+Considere:
+meses = ["Enero","Febrero","Marzo"]
+ventas = [10, 50, 30],
+"""
+
+import pandas as pd
+
+meses = ["Enero","Febrero","Marzo"]
+ventas = [10, 50, 30]
+
+
+df = pd.DataFrame({
+    "ventas" : ventas,  
+    "meses" : meses
+})
+
+
+indice_max = df["ventas"].idxmax()
+df.loc[indice_max]
+print(df)
+print(df["ventas"].idxmax())
+print(df.loc[indice_max]) #quiero la fila cuyo indice sea 1, por eso nos interesó guardar el indice_max
+
