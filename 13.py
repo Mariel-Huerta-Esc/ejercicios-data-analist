73b6d344776f571c6472acc19054b5da13a2edc8
Mariel Huerta Escobar <marielhuerta818@gmail.com>
2026-02-04 19:19:40 -0600

    basic pandas

--- 13.py
+++ 13.py
@@ -28,7 +28,7 @@ print(df["ventas"].max()) """
 
 
 
-
+# ---------------------------------------------------------------------------------------------------
 
 
 
@@ -38,7 +38,7 @@ Este es otro ejercicio:
 Considere:
 meses = ["Enero","Febrero","Marzo"]
 ventas = [10, 50, 30],
-"""
+
 
 import pandas as pd
 
@@ -55,6 +55,33 @@ df = pd.DataFrame({
 indice_max = df["ventas"].idxmax()
 df.loc[indice_max]
 print(df)
-print(df["ventas"].idxmax())
+print(df["ventas"].idxmax()) #esta linea imprime la el indice de donde se encuentra la venta mayor
 print(df.loc[indice_max]) #quiero la fila cuyo indice sea 1, por eso nos interesó guardar el indice_max
+"""
+
+
+# -------------------------------------------------------------------------------------
+
+"""Considere este dataset:
+meses = ["Enero","Febrero","Marzo"]
+ventas = [10, 50, 30],
+
+OBJETIVO:
+obtener SOLO el nombre del mes donde ocurrió la venta mayor.
+"""
+
+
+import pandas as pd
+meses = ["Enero","Febrero","Marzo"]
+ventas = [10, 50, 30]
+
+df = pd.DataFrame({
+    "meses" : meses,
+    "ventas" : ventas
+})
+
+indice_maximo = df["ventas"].idxmax() #guarda el indice de la venta maxima
+df.loc[indice_maximo]
+print(df["ventas"].idxmax()) #imprime valor de agenda máxima
+print(df["meses"].loc[indice_maximo])
 
